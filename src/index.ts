8c31ed7fa954cce12916ba370f236b1743f27a61
evan yuanzh <yuanentai@yuanentaideiMac.local>
2020-03-24 18:56:15 +0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -10,21 +10,21 @@ export interface Onions {
   },
 }
 
-export const compose = (middleware: Array<Function>) =>
-  middleware.reduce((a, b) => (...args: any) => a(b(...args)));
+export const compose = (middlewares: Array<Function>) =>
+  middlewares.reduce((a, b) => (...args: any) => a(b(...args)));
 
 export let onions: Onions;
 onions = (reducers, beforeMiddleware, afterMiddleware) => {
-  const wrapBeforeMiddleware = compose(beforeMiddleware);
-  const wrapAfterMiddleware = compose(afterMiddleware);
+  const wrapBefore = compose(beforeMiddleware);
+  const wrapAfter = compose(afterMiddleware);
 
   return Object.keys(reducers).reduce((resultBundle, item) => {
     resultBundle[item] = (...args) => {
-      const beforeResult = wrapBeforeMiddleware(reducers[item])(...args);
+      const beforeResult = wrapBefore(reducers[item])(...args);
       const beforeCallback = typeof beforeResult === 'function' ?
                              beforeResult : (() => {});
 
-      return wrapAfterMiddleware(beforeCallback)(...args);
+      return wrapAfter(beforeCallback)(...args);
     }
 
     return resultBundle;
